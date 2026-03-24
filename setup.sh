e13af23005a348152a9d0d4798895637590c9382
webreflection <andrea.giammarchi@gmail.com>
2026-03-24 13:40:09 +0100

    tested and failed at importing directly from NPM due missing dependencies resolution

--- setup.sh
+++ setup.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+# # ⚠️ AN NPM PACKAGE WITH DEPENDENCIES CANNOT BE USED DIRECTLY IN HERE
+# fetch_npm_package() {
+#     NAME=$1
+#     if [ -z "$2" ]; then
+#         TAG="latest"
+#     else
+#         TAG=$2
+#     fi
+#     URL=$(curl https://registry.npmjs.org/${NAME} | jq ".versions[.\"dist-tags\".${TAG}].dist.tarball")
+#     echo -e " 🌐 Fetching \x1b[32m${URL:1:-1}\x1b[0m \u2026"
+#     curl -LO ${URL:1:-1}
+# }
+
+# # FROM NPM LATEST TAG (or pass a tag explicitly as 2nd argument)
+# fetch_npm_package "reflected"
+
+# FALLBACK TO MANUALLY EXPORTED FILES VIA GITHUB RAW URLS
 fetch_github_tag_file() {
     REPO=$1
     FILE=$2
@@ -19,10 +36,10 @@ source env/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 
-## FROM GITHUB LATEST TAG (or pass a tag explicitly as 3rd argument)
+# FROM GITHUB LATEST TAG (or pass a tag explicitly as 3rd argument)
 fetch_github_tag_file "WebReflection/reflected" "reflected.tar.gz"
 
-## LATEST FROM CDN  (if published)
+# # LATEST FROM CDN  (if published)
 # curl -LO https://cdn.jsdelivr.net/npm/reflected@latest/reflected.tar.gz
 
 tar -xzf reflected.tar.gz
